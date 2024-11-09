6beb99073638f181237149f907da421e876d5da9
Het Siddhapura <het@aarogram.com>
2024-11-09 10:51:58 +0530

    Implement components structure
    Implment Customize it button

---
+++ client/src/components/index.js
@@ -0,0 +1,7 @@
+import CustomButton from "./CustomButton";
+import AIPicker from "./AIPicker";
+import ColorPicker from "./ColorPicker";
+import FilePicer from "./FilePicker";
+import Tab from "./Tab"; 
+
+export {CustomButton, AIPicker, ColorPicker, FilePicer, Tab};
