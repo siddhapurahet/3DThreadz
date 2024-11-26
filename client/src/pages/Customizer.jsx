c29fc1bfcbd9abb7d71f618d082bd5aeb83b199f
Het Siddhapura <het@aarogram.com>
2024-11-26 10:13:19 -0500

    Implement Frontend features
    Code throwing error in AI module implementation ()Error of importing Configuration

--- client/src/pages/Customizer.jsx
+++ client/src/pages/Customizer.jsx
@@ -1,15 +1,16 @@
 import React, { useState, useEffect } from "react";
 import { AnimatePresence, motion } from "framer-motion";
 import { useSnapshot } from "valtio";
+
 import config from "../config/config";
 import state from "../store";
-import { download, logoShirt, stylishShirt } from "../assets";
+import { download } from "../assets";
 import { downloadCanvasToImage, reader } from "../config/helpers";
 import { EditorTabs, FilterTabs, DecalTypes } from "../config/constants";
 import { fadeAnimation, slideAnimation } from "../config/motion";
 import {
-  ColorPicker,
   AIPicker,
+  ColorPicker,
   CustomButton,
   FilePicker,
   Tab,
@@ -19,15 +20,17 @@ const Customizer = () => {
   const snap = useSnapshot(state);
 
   const [file, setFile] = useState("");
+
   const [prompt, setPrompt] = useState("");
   const [generatingImg, setGeneratingImg] = useState(false);
+
   const [activeEditorTab, setActiveEditorTab] = useState("");
   const [activeFilterTab, setActiveFilterTab] = useState({
     logoShirt: true,
     stylishShirt: false,
   });
 
-  //show TabContent based on the activeTab
+  // show tab content depending on the activeTab
   const generateTabContent = () => {
     switch (activeEditorTab) {
       case "colorpicker":
@@ -35,9 +38,43 @@ const Customizer = () => {
       case "filepicker":
         return <FilePicker file={file} setFile={setFile} readFile={readFile} />;
       case "aipicker":
-        return <AIPicker />;
+        return (
+          <AIPicker
+            prompt={prompt}
+            setPrompt={setPrompt}
+            generatingImg={generatingImg}
+            handleSubmit={handleSubmit}
+          />
+        );
       default:
         return null;
+    }
+  };
+
+  const handleSubmit = async (type) => {
+    if (!prompt) return alert("Please enter a prompt");
+
+    try {
+      setGeneratingImg(true);
+
+      const response = await fetch("http://localhost:8080/api/v1/dalle", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          prompt,
+        }),
+      });
+
+      const data = await response.json();
+
+      handleDecals(type, `data:image/png;base64,${data.photo}`);
+    } catch (error) {
+      alert(error);
+    } finally {
+      setGeneratingImg(false);
+      setActiveEditorTab("");
     }
   };
 
@@ -87,8 +124,8 @@ const Customizer = () => {
       {!snap.intro && (
         <>
           <motion.div
-            className="abcolute top-0 left-0 z-10"
             key="custom"
+            className="absolute top-0 left-0 z-10"
             {...slideAnimation("left")}
           >
             <div className="flex items-center min-h-screen">
@@ -97,9 +134,7 @@ const Customizer = () => {
                   <Tab
                     key={tab.name}
                     tab={tab}
-                    handleClick={() => {
-                      setActiveEditorTab(tab.name);
-                    }}
+                    handleClick={() => setActiveEditorTab(tab.name)}
                   />
                 ))}
 
@@ -108,33 +143,31 @@ const Customizer = () => {
             </div>
           </motion.div>
 
-          <motion.div>
-            <motion.div
-              className="absolute z-10 top-5 right-5"
-              {...fadeAnimation}
-            >
-              <CustomButton
-                type="filled"
-                title="Go Back"
-                handleClick={() => (state.intro = true)}
-                CustomStyles="w-fit px-4 py-2.5 font-bold text-sm"
-              />
-            </motion.div>
+          <motion.div
+            className="absolute z-10 top-5 right-5"
+            {...fadeAnimation}
+          >
+            <CustomButton
+              type="filled"
+              title="Go Back"
+              handleClick={() => (state.intro = true)}
+              customStyles="w-fit px-4 py-2.5 font-bold text-sm"
+            />
+          </motion.div>
 
-            <motion.div
-              className="filtertabs-container"
-              {...slideAnimation("up")}
-            >
-              {FilterTabs.map((tab) => {
-                <Tab
-                  key={tab.name}
-                  tab={tab}
-                  isFilterTab
-                  isActiveTab=""
-                  handleClick={() => {}}
-                />;
-              })}
-            </motion.div>
+          <motion.div
+            className="filtertabs-container"
+            {...slideAnimation("up")}
+          >
+            {FilterTabs.map((tab) => (
+              <Tab
+                key={tab.name}
+                tab={tab}
+                isFilterTab
+                isActiveTab={activeFilterTab[tab.name]}
+                handleClick={() => handleActiveFilterTab(tab.name)}
+              />
+            ))}
           </motion.div>
         </>
       )}
